558276892d5215a687d051b128e463dd74b5cd0f
PrtkPokhrel <pokhrelpratik52@gmail.com>
2024-08-07 11:07:21 +0545

    made mobile version first, added a hamburger menu and made mobile navigation resonsibe

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const hamburger=document.getElementById('hamburger');
+    const navMenu=document.getElementById('nav-menu');
+
+    hamburger.addEventListener('click',()=>{
+        navMenu.classList.toggle('show');
+    })
+
+    navMenu.addEventListener('click',()=>{
+        navMenu.classList.toggle('show');
+    })
+})
+
